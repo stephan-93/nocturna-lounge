47bd6d50d40eebb726456c69d15575a79a7e30d6
Stephan Bradley <eammemp@gmail.com>
2018-03-27 10:21:44 -1000

    First commit
    Pending:
    *Revise section higth on home
    *Fix text box higth on Contact
    *Create resevation form
    *Add Gaming page
    *Add Sing page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //TOGGLE MENU NAV
+    var menu = document.getElementsByClassName("menu");
+    var i;
+    for (i = 0; i < menu.length; i++) {
+        menu[i].onclick = function() {
+            this.classList.toggle("expanded");
+        }
+    }
+})
